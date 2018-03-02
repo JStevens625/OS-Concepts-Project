51a92b2829febcea8b84b1f2cd0886f881ae02db
Derick Vigne <derickvigne@me.com>
2018-03-01 21:33:42 -0500

    Adjusted LFSR Bounds

--- kernel/proc.c
+++ kernel/proc.c
@@ -261,7 +261,7 @@ int randomNum(int pid){
         bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
         lfsr =  (lfsr >> 1) | (bit << 15);
         ++period;
-    } while (lfsr != start_state);
+    } while (lfsr > MAX);
     return 0;
 }
 // Per-CPU process scheduler.
