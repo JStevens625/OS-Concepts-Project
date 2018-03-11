72e953549be7d24f090f9e7d52e2d91976042759
Jacob Stevens <cobymstevens44@gmail.com>
2018-03-11 17:15:41 -0400

    Created null pointer Dereference and modified makefile for runnability.

---
+++ user/null.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+  int *p = 0; //Creates Integer Pointer to NULL
+  printf(1, "Dereference P:[%x] \n", *p);
+  exit();
+}
